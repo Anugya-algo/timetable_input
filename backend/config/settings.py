bd8441d336e85b5fca1fe8f0ce5a02c18b12d7bb
CodeXena <findanugya@gmail.com>
2026-02-09 17:15:05 +0530

    Debug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a-v7tg10k_e-i=cy1mb1(im+_kuuhhxbqg8=_8^ly8pvw=oxn2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'timetable-input.onrender.com']
 
