ce3a87896f5e9f7729f49a85616fd10ba0597162
CodeXena <findanugya@gmail.com>
2026-02-11 23:39:59 +0530

    models

--- backend/documents/admin.py
+++ backend/documents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ReferencePDF
 
-# Register your models here.
+
+@admin.register(ReferencePDF)
+class ReferencePDFAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'note', 'file_size', 'uploaded_at', 'cloudinary_url')
+    list_filter = ('uploaded_at',)
+    search_fields = ('filename', 'note')
+    readonly_fields = ('id', 'cloudinary_public_id', 'cloudinary_url', 'file_size', 'uploaded_at')
+    ordering = ('-uploaded_at',)
